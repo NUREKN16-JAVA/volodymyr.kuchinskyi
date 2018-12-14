cb5ce29b5c50c8be0a985bddb9153ddcc507277c
Kuchinskiy-Vladimir <volodymyr.kuchinskyi@nure.ua>
2018-12-14 10:17:33 +0200

    adds MainFrame#getContentPanel() and #getBrowsePanel() methods

---
+++ src/main/java/ua/nure/kn/kuchinskiy/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,8 @@
+package ua.nure.kn.kuchinskiy.usermanagement.gui;
+
+import javax.swing.*;
+
+public class BrowsePanel extends JPanel {
+    public BrowsePanel(MainFrame mainFrame) {
+    }
+}
--- src/main/java/ua/nure/kn/kuchinskiy/usermanagement/gui/MainFrame.java
+++ src/main/java/ua/nure/kn/kuchinskiy/usermanagement/gui/MainFrame.java
@@ -1,6 +1,40 @@
 package ua.nure.kn.kuchinskiy.usermanagement.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private static final String TITLE = "Managing users";
+    private static final int FRAME_HEIGHT = 600;
+    private static final int FRAME_WIDTH = 800;
+    private JPanel contentPanel;
+    private JPanel browsePanel;
+
+    public MainFrame() {
+        super();
+        initialize();
+    }
+
+    private void initialize() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle(TITLE);
+        this.setContentPane(getContentPanel());
+    }
+
+    private JPanel getContentPanel() {
+        if (contentPanel == null) {
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+        }
+        return contentPanel;
+    }
+
+    private JPanel getBrowsePanel() {
+        if (browsePanel == null) {
+            browsePanel = new BrowsePanel(this);
+        }
+        return browsePanel;
+    }
 }
